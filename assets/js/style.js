8ece182af6d6c630e6bf32fa72eaf8a7044a0de5
balveerdev <balveersolanki0123@gmail.com>
2025-04-22 18:12:38 +0530

    Update style.js

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,79 +1,30 @@
-// document.querySelector('#zipForm').addEventListener('submit', getLocationInfo);
 
-// document.querySelector('body').addEventListener('click', deleteLocation);
-
-// function getLocationInfo(e) {
-//     const zip = document.querySelector('.zip').Value;
-
-//     fetch(`http://api.zippopotam.us/us/${zip}`)
-//         .then(response => {
-//             if (response.status != 200) {
-//                 document.querySelector(`#output`).innerHTML = `<articl class="message is-danger"><div class="message-body">Invalid Zipcode, please try again</div></article>`;
-//                 throw Error(response.statusText);
-//             } else {
-//                 return response.json();
-//             }
-//         })
-//         .then(data => {
-//             let output = '';
-//             data.places.forEach(place => {
-//                 output += `<article class="message is-primary">
-//                 <div class="message-header">
-//                 <P>Location Info</p> 
-//                 <button class="delete"></button>
-//                  </div>
-//                   <div class="message-body">
-//                    <ul>
-//                     <li><strong>City: </strong>${place['place name']} </li> 
-//                      <li><strong>State: </strong>${place['state']} </li> 
-//                       <li><strong>Longitude: </strong>${place['longitude']} </li> 
-//                        <li><strong>Latitude: </strong>${place['latitude']} </li>
-//                         </ul>
-//                          </div>
-//                          </article>`;
-//             });
-
-//             document.querySelector('#output').innerHTML = output;
-//         })
-//         .catch(err => console.log(err));
-
-//     e.preventDefault();
-// }
-
-// function showIcon(icon) {
-//     document.querySelector('.icon-remove').style.display = 'none';
-//     document.querySelector('.icon-check').style.display = 'none';
-//     document.querySelector(`.icon-${icon}`).style.display = 'inline-flex';
-// }
-
-// function deleteLocation(e) {
-//     if (e.target.className == 'delete') {
-//         document.querySelector('.message').remove();
-//         document.querySelector('.zip').value = '';
-//         document.querySelector('.icon-check').remove();
-//     }
-// }
-
-
-
-// ///////////////////////////////////////////////////////////////////////////////
-
-
-
+// Access video and canvas elements
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const context = canvas.getContext('2d');
 
-// Access webcam
-navigator.mediaDevices.getUserMedia({video: true })
-.then(stream => {
-    video.srcobject = stream;
-})
-.catch(error => {
-  alert("Error accessing webcam:" + error);
-});
+// Request webcam access
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => {
+        video.srcObject = stream;
+    })
+    .catch(error => {
+        console.error('Error accessing the webcam:', error);
+    });
 
-// Capture frame
+// Capture the photo and draw it to the canvas
 function capturePhoto() {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
 }
+
+// Save Photo 
+
+function savePhoto() {
+    const image = canvas.toDataURL("image/png"); // Get image data
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'photo.png';
+    link.click();
+}
+
